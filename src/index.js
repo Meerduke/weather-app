f30f7a6dea0505bbf4a2d3cfd1af62aa166af12d
Meerduke <Meerduke@users.noreply.github.com>
2021-02-09 17:20:24 +0100

    Current date and time added

--- src/index.js
+++ src/index.js
@@ -13,22 +13,12 @@ function showTemperature(response){
 
 
 let apiKey = "aae79086babd8e5274d8186968279eae";
-let cityName = 'London';
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
 
 
-
-
-
-
-
-
-
-
 let now = new Date();
-
 let days = [
   "Sunday",
   "Monday",
@@ -38,11 +28,8 @@ let days = [
   "Friday",
   "Saturday"
 ];
-
 let day = days[now.getDay()];
-
 let date = now.getDate();
-
 let months = [
   "January",
   "February",
@@ -57,11 +44,11 @@ let months = [
   "November",
   "December"
 ];
-
 let month = months[now.getMonth()];
-
 let hours = now.getHours();
 let minutes = now.getMinutes();
-
+  if (minutes <10){
+    minutes = `0${minutes}`;
+  }
 let changeDate = document.querySelector(".current-date-time");
 changeDate.innerHTML = `${day} ${date} ${month}, ${hours}:${minutes}`;
