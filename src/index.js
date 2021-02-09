f02d9ba5cdb61020c8c2c3001733bf9e4591986d
Meerduke <Meerduke@users.noreply.github.com>
2021-02-09 16:34:00 +0100

    API integrated

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
-function showTemperature(response) {
-  let temperature = Math.round(response.data.list[0].main.temp);
-  let celsius = document.querySelector("#celsius-button");
-  celsius.innerHTML = `${temperature}°C`;
-}
-
-function showCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let currentCity = document.querySelector("#current-city");
-  currentCity.innerHTML = `${searchInput.value}`;
-  let apiKey = "aae79086babd8e5274d8186968279eae";
-  let currentTempUrl = `https://api.openweathermap.org/data/2.5/find?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(currentTempUrl).then(showTemperature);
-}
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", showCity);
-
-function showCurrentTemp(response) {
-  let cityName = document.querySelector("#current-city");
-  cityName.innerHTML = response.data.list[0].name;
-  let temperature = Math.round(response.data.list[0].main.temp);
-  let celsius = document.querySelector("#celsius-button");
-  celsius.innerHTML = `${temperature}°C`;
-}
-
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let cityCount = 1;
-  let apiKey = "aae79086babd8e5274d8186968279eae";
-  let currentCityUrl = `https://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=${cityCount}&appid=${apiKey}&units=metric`;
-  axios.get(currentCityUrl).then(showCurrentTemp);
-}
-
-function getCurrentPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
+function showTemperature(response){
+  console.log(response.data);
 }
 
 
+let apiKey = "aae79086babd8e5274d8186968279eae";
+let cityName = 'London';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
 
 
 
@@ -49,10 +17,6 @@ function getCurrentPosition(event) {
 
 
 
-
-
-let button = document.querySelector("#current-button");
-button.addEventListener("click", getCurrentPosition);
 
 let now = new Date();
 
